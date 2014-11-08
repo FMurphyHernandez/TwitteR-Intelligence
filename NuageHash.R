f10125253a095c79dcd58fdbd12207d066556c41
Frank Murphy Hernandez <murphy@ciencias.unam.mx>
2014-11-08 10:37:31 -0600

    Create NuageHash.R

---
+++ NuageHash.R
@@ -0,0 +1,18 @@
+
+NuageHash<-function(MOT){
+library("ROAuth")
+library("twitteR")
+library("wordcloud")
+library("tm")
+library(RColorBrewer)
+load(file="twitter authentication.Rdata")
+registerTwitterOAuth(cred)
+Twts<- searchTwitter(paste('#',MOT,sep=''), n=500, cainfo="cacert.pem")
+Twts<- sapply(r_stats, function(x) x$getText())
+Twts <- Corpus(VectorSource(Twts))
+Twts<- tm_map(Twts, content_transformer(tolower))
+Twts<- tm_map(r_stats_text_corpus, removePunctuation)
+r_stats_text_corpus <- tm_map(Twts, function(x)removeWords(x,stopwords()))
+pal2 <- brewer.pal(8,"Dark2")
+return(wordcloud(Twts,min.freq=2,max.words=100, random.order=T, colors=pal2))
+}
