5c349b20e107d7a8edb07823e00ebb0f64afcf74
Frank Murphy Hernandez <murphy@ciencias.unam.mx>
2014-12-11 12:00:52 -0600

    Create DB.R

---
+++ DB.R
@@ -0,0 +1,28 @@
+
+
+k<-1000
+
+library("twitteR")
+library(RODBC)
+load(file="twitter authentication.Rdata")
+registerTwitterOAuth(cred)
+chanel <- odbcConnect("Ral98SN",uid = "TEMPRUSER", pwd = "Mexic.17")
+cadsql<-'select id from dbo.Tweets'
+Train<- sqlQuery(chanel, cadsql)
+Train<-as.vector(Train)
+Train<-as.character(Train)
+
+
+	Twts<- searchTwitter('movistarmx OR telcel OR unefon OR iusacell OR nextel',n=k,lang='es', retryOnRateLimit=5,cainfo="cacert.pem")
+	Twts<-twListToDF(Twts)
+	Proof<-Twts[Twts$isRetweet==FALSE,]
+	Proof<-Proof[,c(1,8)]
+	nrow(Proof)
+	Proof<-Proof[!(Proof$id %in% Train),]
+	nrow(Proof)
+
+
+
+sqlSave(chanel,Proof, "tmp_Tweets",append=TRUE,rownames=FALSE)
+sqlQuery(chanel,'dbo.SP_tweets_sent')
+close(chanel)
